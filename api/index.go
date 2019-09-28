dd55e90f92a2ddf78146beb8457b27303776b136
Hung V. Nguyen <ng.hung83@gmail.com>
2019-09-28 14:47:45 +0700

    Proxy fshare

--- api/index.go
+++ api/index.go
@@ -1,10 +1,18 @@
 package handler
 
 import (
-  "fmt"
-  "net/http"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "<h1>Hello from Go on Now!</h1>")
+	resp, _ := http.Get("https://www.fshare.vn/site/login")
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Fprintf(w, string(b))
 }
